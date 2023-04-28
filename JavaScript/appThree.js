f4653b813ad4c7687019f6fdc07c12009a568796
70561_VitorNunes <al70561@utad.eu>
2023-04-28 18:41:35 +0100

    tutorial 6 + "scene"

--- JavaScript/appThree.js
+++ JavaScript/appThree.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {FBXLoader}  from 'FBXLoader';
+import { PointerLockControls } from 'PointerLockControls';
  
 document.addEventListener('DOMContentLoaded', Start);
 
@@ -8,7 +9,7 @@ var camara = new THREE.OrthographicCamera(-1 ,1, 1, -1, -10 , 10);
 var renderer = new THREE.WebGLRenderer();
 var camaraPerspetiva = new THREE.PerspectiveCamera(45,4/3,0.1,100);
 
-renderer.setSize(window.innerHeight +900, window.innerWidth -1000);
+renderer.setSize(window.innerHeight-15, window.innerWidth-80);
 renderer.setClearColor(0xaaaaaa);
 
 document.body.appendChild(renderer.domElement);
@@ -24,14 +25,14 @@ meshCubo.translateZ(-6.0);
 var objetoImportado;
 var mixerAnimacao;
 var relogio = new THREE.Clock();
-const importer = new THREE.FBXLoader();
+var importer = new FBXLoader();
 
-importer.load('./Objetos/Samba Dancing.fbx', function (object) {
+importer.load('./Objetos/dusty-path-in-the-fields/source/the_way/the_way.fbx', function (object) {
    
     mixerAnimacao = new THREE.AnimationMixer(object);
     
-    var action = mixerAnimacao.clipAction(object.animations [0]);
-    action.play();
+    //var action = mixerAnimacao.clipAction(object.animations[0]);
+    //action.play();
     
     object.traverse(function (child){
         if(child.isMesh){
@@ -40,9 +41,11 @@ importer.load('./Objetos/Samba Dancing.fbx', function (object) {
         }
     });
 
-    object.scale.x=0.01;
-    object.scale.z=0.01;
-    object.scale.y=0.01;
+    cena.add(object);
+
+    object.scale.x=0.0001;
+    object.scale.z=0.0001;
+    object.scale.y=0.0001;
 
     object.position.x=1.5;
     object.position.y=-0.5;
@@ -50,6 +53,44 @@ importer.load('./Objetos/Samba Dancing.fbx', function (object) {
 
     objetoImportado = object;
 });
+
+const controls = new PointerLockControls(camaraPerspetiva,renderer.domElement)
+controls.addEventListener('lock', function(){});
+controls.addEventListener('unlock', function(){});
+
+document.addEventListener(
+    'click',
+    function(){
+        controls.lock()
+    },
+    false   
+);
+
+
+document.addEventListener("keydown", onDocumentKeyDown, false);
+function onDocumentKeyDown(event){
+    var keyCode = event.which;
+    if(keyCode == 87){
+        controls.moveForward(0.25)
+    } 
+    else if(keyCode == 83){
+        controls.moveForward(-0.25)
+    }
+    else if(keyCode == 65){
+        controls.moveRight(-0.25)
+    }
+    else if(keyCode == 68){
+        controls.moveRight(0.25)
+    }
+    else if(keyCode == 32){
+        if(meshCubo.parent ===cena){
+            cena.remove(meshCubo);
+        }
+        else{
+            cena.add(meshCubo);
+        }
+    }
+}
 
 function Start(){
 
@@ -66,15 +107,39 @@ function Start(){
 
     renderer.render(cena,camaraPerspetiva);
     requestAnimationFrame(loop);
+
+    var texture_dir = new THREE.TextureLoader().load('./Images/sky.jpg');
+    var texture_esq = new THREE.TextureLoader().load('./Images/sky.jpg');
+    var texture_up = new THREE.TextureLoader().load('./Images/sky.jpg');
+    var texture_dn = new THREE.TextureLoader().load('./Images/terra.jpg');
+    var texture_bk = new THREE.TextureLoader().load('./Images/sky.jpg');
+    var texture_ft = new THREE.TextureLoader().load('./Images/sky.jpg');
+
+    var materialArray = [];
+
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dir}));
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_esq}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_up}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_dn}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_bk}))
+    materialArray.push(new THREE.MeshBasicMaterial({map: texture_ft}))
+
+
+    for (var i=0; i<6; i++)
+        materialArray[i].side = THREE.BackSide;
+
+    var skyboxGeo = new THREE.BoxGeometry(100,100,100);
+    var skybox = new THREE.Mesh(skyboxGeo,materialArray);
+    cena.add(skybox);
 }
 
 function loop(){
 
     meshCubo.rotateY(Math.PI/180*1);
 
-    if(mixerAnimacao){
-        mixerAnimacao.update(relogio,getDelta());
-    }
+    //f(mixerAnimacao){
+      //  mixerAnimacao.update(relogio.getDelta());
+    //}
 
     renderer.render(cena,camaraPerspetiva);
 
